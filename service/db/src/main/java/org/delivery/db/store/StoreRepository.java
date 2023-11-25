fe5c01f0513a8cd8c36173b96eef6e36d8667e08
DY LEE <dleo238@gmail.com>
2023-11-25 13:02:24 +0900

    add store storemenu user jpa

---
+++ service/db/src/main/java/org/delivery/db/store/StoreRepository.java
@@ -0,0 +1,23 @@
+package org.delivery.db.store;
+
+import org.delivery.db.store.enums.StoreCategory;
+import org.delivery.db.store.enums.StoreStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+
+	// valid store one
+	// select * from store where id = ? and status = ? order by id desc limit 1
+	Optional<StoreEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, StoreStatus status);
+
+	// valid store list
+	// select * from store where status = ? order by id desc
+	List<StoreEntity> findAllByStatusOrderByIdDesc(StoreStatus status);
+
+	// valid store list by category
+	// select * from store where status = ? and category = ? order by star desc
+	List<StoreEntity> findAllByStatusAndCategoryOrderByStarDesc(StoreStatus storeStatus, StoreCategory storeCategory);
+}
